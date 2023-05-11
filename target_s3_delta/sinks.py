66314f3c0718b536e78a178b1a4956df27df8d57
resulyurttakalan <resul.yurttakalan@getir.com>
2023-05-12 00:50:21 +0300

    check mysql typed none datetime fields

--- target_s3_delta/sinks.py
+++ target_s3_delta/sinks.py
@@ -2,15 +2,78 @@
 
 from __future__ import annotations
 
+from dateutil import parser
 from deltalake import write_deltalake
+from singer_sdk.helpers._typing import (
+    DatetimeErrorTreatmentEnum,
+    get_datelike_property_type,
+    handle_invalid_timestamp_in_record,
+)
 from singer_sdk.sinks import BatchSink
 import pandas as pd
+
+NOT_PROPER_DATETIME_FORMAT = "0000-00-00 00:00:00"
 
 
 class S3DeltaSink(BatchSink):
     """s3-delta target sink class."""
 
     max_size = 10000  # Max records to write in one batch
+
+    def process_record(self, record: dict, context: dict) -> None:
+        """Load the latest record from the stream.
+        Args:
+            record: Individual record in the stream.
+            context: Stream partition or context dictionary.
+        """
+        for key in record:
+            date_val = record[key]
+            if date_val == NOT_PROPER_DATETIME_FORMAT:
+                record[key] = None
+
+        if "records" not in context:
+            context["records"] = []
+
+        context["records"].append(record)
+
+    def _parse_timestamps_in_record(
+        self,
+        record: dict,
+        schema: dict,
+        treatment: DatetimeErrorTreatmentEnum,
+    ) -> None:
+        """Parse strings to datetime.datetime values, repairing or erroring on failure.
+
+        Attempts to parse every field that is of type date/datetime/time. If its value
+        is out of range, repair logic will be driven by the `treatment` input arg:
+        MAX, NULL, or ERROR.
+
+        Args:
+            record: Individual record in the stream.
+            schema:
+            treatment:
+        """
+        for key in record:
+            datelike_type = get_datelike_property_type(schema["properties"][key])
+            if datelike_type:
+                date_val = record[key]
+                try:
+                    if record[key] is not None:
+                        if date_val == NOT_PROPER_DATETIME_FORMAT:
+                            record[key] = None
+                        else:
+                            date_val = parser.parse(date_val)
+                except parser.ParserError as ex:
+                    date_val = handle_invalid_timestamp_in_record(
+                        record,
+                        [key],
+                        date_val,
+                        datelike_type,
+                        ex,
+                        treatment,
+                        self.logger,
+                    )
+                record[key] = date_val
 
     def start_batch(self, context: dict) -> None:
         """Start a batch.
